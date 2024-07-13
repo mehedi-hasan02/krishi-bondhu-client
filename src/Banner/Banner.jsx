acf9b618f0a3b9e5d032ca94fe7114e5a23efd7a
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2024-07-14 01:52:03 +0600

    some improvement to navbar

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -2,9 +2,9 @@ import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const Banner = () => {
   return (
-    <div className="h-lvh banner_background">
-      <Navbar></Navbar>
-      <div>
+    <div className=" ">
+      <div className="h-lvh banner_background absolute w-full">
+        <Navbar className="relative top-0"></Navbar>
         <div className="flex flex-col gap-3 mt-14">
           <h1 className="text-3xl text-white mx-auto">
             Welcome to Krishi Bondhu
