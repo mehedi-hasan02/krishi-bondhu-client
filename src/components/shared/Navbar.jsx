3f1860ce3a3c0bbf81c0d11859fa15ede95d8349
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2024-07-14 04:09:21 +0600

    Added search modal in the Navbar component

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,23 +2,24 @@ import { GiFallingLeaf } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.jpg";
 import { PiSignInFill, PiSignOutBold } from "react-icons/pi";
+import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
   const navLinks = (
     <>
-      <li className="text-green-500 hover:text-white rounded-md ">
+      <li className="text-green-500 hover:text-yellow-500 rounded-md ">
         <Link to="/">Home</Link>
       </li>
-      <li className="text-green-500 hover:text-white rounded-md  ">
+      <li className="text-green-500 hover:text-yellow-500 rounded-md  ">
         <Link to="/about">About</Link>
       </li>
-      <li className="text-green-500 hover:text-white rounded-md  ">
+      <li className="text-green-500 hover:text-yellow-500 rounded-md  ">
         <Link to="/contact">Contact</Link>
       </li>
-      <li className="text-green-500 hover:text-white rounded-md  ">
+      <li className="text-green-500 hover:text-yellow-500 rounded-md  ">
         <Link to="/myCart">My Cart</Link>
       </li>
-      <li className="text-green-500 hover:text-white rounded-md  ">
+      <li className="text-green-500 hover:text-yellow-500 rounded-md  ">
         <Link to="/dashboard">Dashboard</Link>
       </li>
     </>
@@ -47,7 +48,33 @@ const Navbar = () => {
           <ul className="menu menu-horizontal px-1">{navLinks}</ul>
         </div>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end flex items-center gap-4">
+        {/* search modal */}
+        <button
+          className=" text-black hover:text-yellow-700 flex items-center"
+          onClick={() => document.getElementById("my_modal_5").showModal()}
+        >
+          <CiSearch></CiSearch>
+        </button>
+        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle ">
+          <div className="modal-box flex flex-col gap-3">
+            <h3 className="font-bold text-lg">Search your Keyword</h3>
+
+            <form method="dialog">
+              <input
+                className="p-3 bg-black text-white w-full rounded-md"
+                type="search"
+                name="search"
+              />
+              <div className="flex justify-between mt-3">
+                <button className="btn btn-success text-white">Search</button>
+
+                <button className="btn btn-warning">X</button>
+              </div>
+            </form>
+          </div>
+        </dialog>
+
         <Link
           className="btn btn-sm text-green-600 bg-inherit border-none"
           to="/login"
