d4c4f416e5561854486659d17a1aea099c2658ae
Fahmidur Rahaman Shanto <118394676+fahmidurshanto@users.noreply.github.com>
2024-07-15 16:58:49 +0600

    Update Main.jsx

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Pages/Shared/Navbar/Navbar";
+import { ToastContainer } from "react-toastify";
 
 const Main = () => {
   return (
     <div>
-      <Navbar />
-      <Outlet />
+      <Navbar/>
+      <Outlet/>
+      <ToastContainer/>
     </div>
   );
 };
